cebd6c1bd68e2660fdfab17869f0a8a846793a10
100jm <bjm.931026@gmail.com>
2022-10-10 23:21:32 +0900

    영화 정보 페이지 진행중

---
+++ src/routes/Detail.js
@@ -0,0 +1,29 @@
+import {useParams} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+
+function Detail() {
+    const {id} = useParams();
+    const [movieInfo, setMovieInfo] = useState({});
+
+    const getMoiveDetail = async() => {
+        const json = await(
+            await fetch(
+                `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+            )
+        ).json();
+        
+        setMovieInfo(json.data.movie);
+        
+    };
+
+    useEffect(() => {
+        getMoiveDetail();
+    }, [])
+    
+    return <h1>
+                <a href={movieInfo.url} target='blank'>{movieInfo.title}</a>
+            </h1>;
+
+}
+
+export default Detail;
