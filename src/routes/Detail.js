35f51435d8c192d23a5b03486fd7183f83a8179c
100jm <bjm.931026@gmail.com>
2022-11-24 15:48:52 +0900

    상세 정보 페이지 및 style 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,11 @@
 import {useParams} from 'react-router-dom';
 import {useState, useEffect} from 'react';
+import styles from './Detail.module.css';
 
 function Detail() {
     const {id} = useParams();
     const [movieInfo, setMovieInfo] = useState({});
+    const [loading, setLoading] = useState(true);
 
     const getMoiveDetail = async() => {
         const json = await(
@@ -13,17 +15,60 @@ function Detail() {
         ).json();
         
         setMovieInfo(json.data.movie);
-        console.log(json.data.movie);
-        
+        setLoading(false);
     };
 
     useEffect(() => {
         getMoiveDetail();
     }, [])
     
-    return <h1>
-                <a href={movieInfo.url} target='blank'>{movieInfo.title}</a>
-            </h1>;
+    return (
+            <div>
+                <div className={styles.backgound}> 
+                    <img className={styles.detail_img} src={movieInfo.background_image_original} alt=""/>
+                </div>
+                {
+                    loading ?
+                    <div className={styles.loader}>
+                        <span>Loading...</span>
+                    </div>
+                    :
+                    <div className={styles.detail_info}>
+                        <div className={styles.info}>
+                            <div className={styles.info_img}>
+                                <img src={movieInfo.medium_cover_image} alt=""/>
+                            </div>
+                            <div className={styles.info_movie}>
+                                <h3>
+                                    {movieInfo.title}
+                                </h3>
+                                <p>{movieInfo.rating ? `Rating : ${movieInfo.rating} / 10` : 'Rating : -'}</p>
+                                <p>{movieInfo.runtime ? `Runtime : ${movieInfo.runtime} min` : 'Runtime : -'}</p>
+                                {
+                                    movieInfo.genres ?
+                                        <div>
+                                            <span>Genres</span>
+                                            <ul>
+                                            {movieInfo.genres.map(g=><li key={g}>{g}</li>)}
+                                            </ul>
+                                        </div>
+                                        :
+                                        null
+                                }
+                            </div>
+                        </div>
+                        {
+                            movieInfo.description_full ?
+                            <div className={styles.description}>
+                                <p>{movieInfo.description_full}</p>
+                            </div>
+                            :
+                            null
+                        }
+                    </div>
+                }
+            </div>
+            );
 
 }
 
